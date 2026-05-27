908c50fdfc117230b26d723ea81475c66ce36ca1
tejaswi-kunal <tejaswi.kunal.in@gmail.com>
2026-05-27 18:00:10 +0530

    Redis Bug Resolved

--- src/config/redis.js
+++ src/config/redis.js
@@ -6,7 +6,13 @@ const redisClient = redis.createClient({
     socket: {
         host: process.env.REDIS_SOCKET_HOST,
         port: Number(process.env.REDIS_SOCKET_PORT)
-    }
+    },
+    pingInterval: 1000 * 60 * 4 // Pings the server every 4 minutes
+});
+
+// Add this right after you initialize your redisClient in config/redis.js
+redisClient.on('error', (err) => {
+    console.error('Redis Client Error:', err.message);
 });
 
 module.exports=redisClient;

1d32166a14fa82f396c17afb55b05a1490d5bb32
Tejaswi Kunal <tejaswi.kunal.in@gmail.com>
2026-06-23 05:43:04 +0530

    issue->nodemailer

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,42 +1,20 @@
 const nodemailer = require("nodemailer");
 
-console.log("EMAIL_USER =", process.env.EMAIL_USER);
-console.log("EMAIL_PASS exists =", !!process.env.EMAIL_PASS);
-
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
 });
 
-transporter.verify((error, success) => {
-    if(error){
-        console.error("TRANSPORTER VERIFY ERROR:", error);
-    }
-    else{
-        console.log("MAIL SERVER READY");
-    }
-});
-
 const sendEmail = async ({to, subject, html}) => {
-    try{
-        const info = await transporter.sendMail({
-            from: process.env.EMAIL_USER,
-            to,
-            subject,
-            html
-        });
-
-        console.log("EMAIL SENT:", info.messageId);
-
-    }catch(err){
-        console.error("SEND MAIL ERROR:", err);
-        throw err;
-    }
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to,
+        subject,
+        html
+    });
 };
 
 module.exports = sendEmail;

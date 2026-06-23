5f3094a995c7f15bca9b44bd58e8abe86c793e97
Tejaswi Kunal <tejaswi.kunal.in@gmail.com>
2026-06-23 05:34:58 +0530

    dubugging email

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -4,21 +4,15 @@ console.log("EMAIL_USER =", process.env.EMAIL_USER);
 console.log("EMAIL_PASS exists =", !!process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
 });
 
-transporter.verify((error, success) => {
-    if(error){
-        console.error("TRANSPORTER VERIFY ERROR:", error);
-    }
-    else{
-        console.log("MAIL SERVER READY");
-    }
-});
 
 const sendEmail = async ({to, subject, html}) => {
     try{
